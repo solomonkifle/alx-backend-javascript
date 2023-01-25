873af7697bb4bcf554d1ffa6ad52453e9fc9d77a
solomonkifle <102066454+solomonkifle@users.noreply.github.com>
2023-01-25 19:19:26 +0300

    Update 0-classroom.js

--- 0x02-ES6_classes/0-classroom.js
+++ 0x02-ES6_classes/0-classroom.js
@@ -2,4 +2,14 @@ export default class ClassRoom {
   constructor(maxStudentsSize) {
     this._maxStudentsSize = maxStudentsSize;
   }
+
+  set maxStudentsSize(newNumber) {
+    if (typeof newNumber === 'number') {
+      this._maxStudentsSize = newNumber;
+    }
+  }
+
+  get maxStudentsSize() {
+    return this._numberStudents;
+  }
 }
