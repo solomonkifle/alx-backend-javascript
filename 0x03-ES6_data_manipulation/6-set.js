81cc02e9edc37ea9b52486986e2311af75c25595
solomonkifle <102066454+solomonkifle@users.noreply.github.com>
2023-01-31 21:38:56 +0300

    Update 6-set.js

--- 0x03-ES6_data_manipulation/6-set.js
+++ 0x03-ES6_data_manipulation/6-set.js
@@ -1,13 +1,3 @@
-/*
- * File: 6-set.js
- * Auth: Yovo Koffi Vianney
- */
-
-/**
- * setFromArray - Creates a set from an array.
- * @array: Array, of any kind of element.
- * Return: A Set from an array.
- */
-export default function setFromArray(array) {
-  return new Set(array);
+export default function setFromArray(arr) {
+  return new Set(arr);
 }
