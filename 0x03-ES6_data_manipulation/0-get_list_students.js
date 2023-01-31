e9016758dfc787dfaca43a117e5bd387590cbcef
solomonkifle <102066454+solomonkifle@users.noreply.github.com>
2023-01-31 21:54:32 +0300

    Update 0-get_list_students.js

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,7 +1,3 @@
-export default function getListStudents() {
-  return [
-    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
-    { id: 2, firstName: 'James', location: 'Columbia' },
-    { id: 5, firstName: 'Serena', location: 'San Francisco' },
-  ];
-}
+import getListStudents from "../0-get_list_students.js";
+
+console.log(getListStudents());
