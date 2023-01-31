6581b4ec4017acdcbb9c073306f50d25e79329b0
solomonkifle <102066454+solomonkifle@users.noreply.github.com>
2023-01-31 21:52:57 +0300

    Update and rename 100-main.js2-main.js to 100-main.js

--- 0x03-ES6_data_manipulation/tests/100-main.js
+++ 0x03-ES6_data_manipulation/tests/100-main.js
@@ -1,4 +1,4 @@
-import { queryAPI, weakMap } from "./100-weak.js";
+import { queryAPI, weakMap } from "../100-weak.js";
 
 const endpoint = { protocol: 'http', name: 'getUsers' };
 weakMap.get(endpoint);
@@ -10,6 +10,13 @@ queryAPI(endpoint);
 console.log(weakMap.get(endpoint));
 
 queryAPI(endpoint);
+console.log(weakMap.get(endpoint));
+
 queryAPI(endpoint);
+console.log(weakMap.get(endpoint));
+
 queryAPI(endpoint);
+console.log(weakMap.get(endpoint));
+
 queryAPI(endpoint);
+console.log(weakMap.get(endpoint));
