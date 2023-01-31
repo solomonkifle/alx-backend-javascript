0b736f3af8c52ef7c01e353963f2f7844066b01c
solomonkifle <102066454+solomonkifle@users.noreply.github.com>
2023-01-31 21:37:40 +0300

    Update 4-update_grade_by_city.js

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,24 +1,16 @@
-/*
- * File: 4-update_grade_by_city.js
- * Auth: Yovo Koffi Vianney
- */
-
-/**
- * updateStudentGradeByCity - Retrieves a list of students by city with their grades.
- * @studentList: A list of students.
- * @city: Student's location.
- * @newGrades:Student's grade.
- * Return: An array of students for a specific city with their new grade.
- */
-export default function updateStudentGradeByCity(studentList, city, newGrades) {
-  return studentList
-    .filter((item) => item.location === city)
-    .map((student) => {
-      const grades = newGrades.filter((item) => item.studentId === student.id);
-      const grade = grades.length ? grades[0].grade : 'N/A';
-      return {
-        ...student,
-        grade,
-      };
-    });
+export default function updateStudentGradeByCity(listStudents, city, newGrades) {
+  const students = listStudents.filter((student) => student.location === city);
+  for (const student of students) {
+    student.grade = 'N/A';
+  }
+  const newStudents = students.map((student) => {
+    const single = student;
+    for (const grade of newGrades) {
+      if (single.id === grade.studentId) {
+        single.grade = grade.grade;
+      }
+    }
+    return single;
+  });
+  return newStudents;
 }
