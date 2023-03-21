e72fea2b786fe4358f63355e803bac093c205388
solomonkifle <102066454+solomonkifle@users.noreply.github.com>
2023-03-21 21:00:16 +0400

    Update 0-console.js

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,5 +1,11 @@
-function displayMessage(data) {
-  console.log(data);
-}
+/**
+ * Executing basic javascript with Node JS
+ * 
+ * Displays a message on the STDOUT.
+ * @param {String} message The message to display.
+ */
+const displayMessage = (message) => {
+  console.log(message);
+};
 
 module.exports = displayMessage;
